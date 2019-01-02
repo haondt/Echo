b77c5b57d51b91c262e7f24c747f53b2a49eb6da
haondt <burghardtnoah@gmail.com>
2019-01-02 13:43:37 -0700

    updated basics to runnable bot skeleton

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Echo
 Echo is a Discord bot, written in Python that provides a variety of functions.
+
+Token should be stored in a single line file in the root directory called "token.txt"
+
+###Requirements
+* discord.py
+
+[Invite link](https://discordapp.com/api/oauth2/authorize?scope=bot&client_id=530116202069098496)
---
+++ main.py
@@ -0,0 +1,17 @@
+# Echo is a Discord bot
+
+import discord
+
+with open('token.txt') as tokenfile:
+	token = tokenfile.read().strip()
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+	# don't let bot reply to itself
+	if message.author != client.user:
+		if message.content.startswith('!hello'):
+			await client.send_message(message.channel, 'Moshi moshi')
+
+client.run(token)
